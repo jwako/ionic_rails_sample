9be61ebbbfcc108f66305095166535fdd1e56924
Junya Wako <j.wako@j-q.co.jp>
2015-03-14 15:01:26 +0900

    Fix config

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
         constants: {
           ENV: {
             name: 'development',
-            apiEndpoint: 'http://dev.yoursite.com:10000/'
+            apiEndpoint: 'http://localhost:3000/api/v1'
           }
         }
       },
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
         constants: {
           ENV: {
             name: 'production',
-            apiEndpoint: 'http://api.yoursite.com/'
+            apiEndpoint: 'https://rails-angular-auth-demo.herokuapp.com/api/v1'
           }
         }
       }
